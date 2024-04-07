c336177b881b1933e7a2ebba861b25bda406a956
Ignacio Pieve <ignacio.pieve@bigbox.com.ar>
2024-04-07 18:16:13 -0300

    styling

--- src/apps/app/Table/head.js
+++ src/apps/app/Table/head.js
@@ -6,8 +6,8 @@ export default function Head() {
             <th className={"text-start"} scope={"col"}>Fecha</th>
             <th className={"text-start"} scope={"col"}>Categoria</th>
             <th className={"text-start"} scope={"col"}>Descripcion</th>
-            <th className={"text-start"} scope={"col"}>Monto</th>
-            <th className={"text-start"} scope={"col"}>Acciones</th>
+            <th className={"text-center"} scope={"col"}>Monto</th>
+            <th className={"text-center"} scope={"col"}>Acciones</th>
         </tr>
     );
 }
